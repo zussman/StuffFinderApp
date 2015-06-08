1f93062a176993982ced55fdbfb2eadb3d4aac19
David Zussman <david.zussman@gmail.com>
2015-06-08 19:10:33 -0400

    Add Classifieds Controller methods for new, index, and show

--- StuffFinderApp/app/controllers/classifieds_controller.rb
+++ StuffFinderApp/app/controllers/classifieds_controller.rb
@@ -1,2 +1,15 @@
 class ClassifiedsController < ApplicationController
+
+  def index
+    @classifieds = Classified.all
+  end
+
+  def show
+    @classified = Classified.find(params[:id])
+  end
+
+  def new
+    @classified = Classified.new
+  end
+
 end
