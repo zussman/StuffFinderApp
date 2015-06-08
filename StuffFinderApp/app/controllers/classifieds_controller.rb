9bb396fc1b13a29ae61b0b5c908432419b2530a4
David Zussman <david.zussman@gmail.com>
2015-06-08 19:34:01 -0400

    Create classified new and show views

--- StuffFinderApp/app/controllers/classifieds_controller.rb
+++ StuffFinderApp/app/controllers/classifieds_controller.rb
@@ -12,4 +12,18 @@ class ClassifiedsController < ApplicationController
     @classified = Classified.new
   end
 
+  def create
+    @classified = Classified.new classified_params
+
+    if @classified.save
+      redirect_to @classified
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def classified_params
+      params.require(:classified).permit(:title, :description, :price, :category_id)
+    end
 end
